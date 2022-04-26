573f037d9e922041be91f0852fdc2d1ab9ebaf35
pook <ijklm@osijklm.local>
2022-04-27 00:12:21 +0200

    last

--- index.php
+++ index.php
@@ -8,13 +8,13 @@
 //http://localhost:8000/index.php?prenom=%3Cscript%3Ealert(%22hacking%20ok%20!%22)%3C/script%3E
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Document</title>
+    <title>index</title>
     <style>
         .formStyle {
             padding: 10px 0;
